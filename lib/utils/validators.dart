a86ed08b94322ff3acee9524ba0b2ef2c3fcb5a0
nelson amos <nelsonamos52@gmail.com>
2025-02-17 19:47:15 +0400

    hi

--- lib/utils/validators.dart
+++
@@ -1,8 +0,0 @@
-bool isValidEmail(String email) {
-  final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
-  return emailRegex.hasMatch(email);
-}
-
-bool isValidPassword(String password) {
-  return password.length >= 6;
-}
